e262fbbb9bb8f478806041ccbcad179407af724e
mauro sorgatto <mauro.sorgatto@azzero.tec.br>
2023-01-27 16:37:13 -0300

    arrumando a fonte

--- src/components/TimeLabel.jsx
+++ src/components/TimeLabel.jsx
@@ -5,7 +5,7 @@ export const TimeLabel = () => (
   <View className="flex flex-row absolute z-10 left-4 bottom-4">
     <View className="flex flex-row align-middle rounded-sm py-1 px-4 bg-white">
       <HourglassIcon />
-      <Text className="ml-2">13:55</Text>
+      <Text className="ml-2 font-inter-600">13:55</Text>
     </View>
   </View>
 );

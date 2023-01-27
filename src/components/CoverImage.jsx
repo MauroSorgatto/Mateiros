38f42ba1e57835c98bfbb86d8606b3020d28fac4
Bruno Azzi <bruno.azzi@icloud.com>
2023-01-27 13:12:25 -0300

    feat: add TimeLabel component, indent CoverImage file

--- src/components/CoverImage.jsx
+++ src/components/CoverImage.jsx
@@ -1,6 +1,14 @@
-import { Image, View } from 'react-native'
-import coverImage from '../../assets/cover.png'
+import { Image, View } from "react-native";
+import coverImage from "../../assets/cover.png";
+import { TimeLabel } from "./TimeLabel";
 
-export const CoverImage = ({ compact }) => <View>
-    <Image className={`flex-initial rounded-t w-full ${compact ? 'h-32' : ''}`} source={coverImage} resizeMode="cover" />
-    </View>
+export const CoverImage = ({ compact }) => (
+  <View>
+    <Image
+      className={`flex-initial rounded-t w-full ${compact ? "h-32" : ""}`}
+      source={coverImage}
+      resizeMode="cover"
+    />
+    <TimeLabel />
+  </View>
+);
---
+++ src/components/TimeLabel.jsx
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+
+export const TimeLabel = () => (
+  <View>
+    <Text>13:55</Text>
+  </View>
+);

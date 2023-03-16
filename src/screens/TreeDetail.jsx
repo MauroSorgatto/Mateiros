d11201400d0a359562c793700711c6c81d26b268
Bruno Azzi <bruno.azzi@icloud.com>
2023-03-16 11:47:15 -0300

    feat: add example of how to access tree data inside page

--- src/screens/TreeDetail.jsx
+++ src/screens/TreeDetail.jsx
@@ -1,1 +1,11 @@
-export const TreeDetail = () => <></>;
+import { Text } from "react-native";
+
+export const TreeDetail = ({ route }) => {
+  const { tree } = route.params;
+
+  return (
+    <>
+      <Text>{tree.name}</Text>
+    </>
+  );
+};
